0268f8ec452d276fe070348fa4551836bb0f64c2
Neville Aga <neville@agafamily.com>
2025-08-14 13:17:38 -0500

    Clean up CSS: moved styles to external style sheet and removed unused classes

--- src/www/2024NcfRatingBiases.php
+++ src/www/2024NcfRatingBiases.php
@@ -2,159 +2,9 @@
 
 <head>
 <meta name=Title content="2024 ncf playoff committee rating biases">
-<meta name=Keywords
-content="ncf playoff committee rating biases 2024">
+<meta name=Keywords content="ncf playoff committee rating biases 2024">
 <title>2024 ncf playoff committee rating biases</title>
-<style>
-<table
-	{mso-displayed-decimal-separator:"\.";
-	mso-displayed-thousand-separator:"\,";}
-@page
-	{margin:.75in .7in .75in .7in;
-	mso-header-margin:.3in;
-	mso-footer-margin:.3in;}
-.style18
-	{background:#F2F2F2;
-	mso-pattern:black none;
-	color:#FA7D00;
-	font-size:11.0pt;
-	font-weight:700;
-	font-style:normal;
-	text-decoration:none;
-	font-family:Calibri, sans-serif;
-	mso-font-charset:0;
-	border:.5pt solid #7F7F7F;
-	mso-style-name:Calculation;
-	mso-style-id:22;}
-.style16
-	{background:#C6EFCE;
-	mso-pattern:black none;
-	color:#006100;
-	font-size:11.0pt;
-	font-weight:400;
-	font-style:normal;
-	text-decoration:none;
-	font-family:Calibri, sans-serif;
-	mso-font-charset:0;
-	mso-style-name:Good;
-	mso-style-id:26;}
-.style17
-	{background:#FFEB9C;
-	mso-pattern:black none;
-	color:#9C6500;
-	font-size:11.0pt;
-	font-weight:400;
-	font-style:normal;
-	text-decoration:none;
-	font-family:Calibri, sans-serif;
-	mso-font-charset:0;
-	mso-style-name:Neutral;
-	mso-style-id:28;}
-.style0
-	{mso-number-format:General;
-	text-align:general;
-	vertical-align:bottom;
-	white-space:nowrap;
-	mso-rotate:0;
-	mso-background-source:auto;
-	mso-pattern:auto;
-	color:black;
-	font-size:11.0pt;
-	font-weight:400;
-	font-style:normal;
-	text-decoration:none;
-	font-family:Calibri, sans-serif;
-	mso-font-charset:0;
-	border:none;
-	mso-protection:locked visible;
-	mso-style-name:Normal;
-	mso-style-id:0;}
-td
-	{mso-style-parent:style0;
-	padding-top:1px;
-	padding-right:1px;
-	padding-left:1px;
-	mso-ignore:padding;
-	color:black;
-	font-size:11.0pt;
-	font-weight:400;
-	font-style:normal;
-	text-decoration:none;
-	font-family:Calibri, sans-serif;
-	mso-font-charset:0;
-	mso-number-format:General;
-	text-align:general;
-	vertical-align:bottom;
-	border:none;
-	mso-background-source:auto;
-	mso-pattern:auto;
-	mso-protection:locked visible;
-	white-space:nowrap;
-	mso-rotate:0;}
-.xl65
-	{mso-style-parent:style17;
-	color:#9C6500;
-	text-align:center;
-	background:#FFEB9C;
-	mso-pattern:black none;}
-.xl66
-	{mso-style-parent:style16;
-	color:#006100;
-	text-align:center;
-	background:#C6EFCE;
-	mso-pattern:black none;}
-.xl67
-	{mso-style-parent:style0;
-	mso-number-format:"0\.000";}
-.xl68
-	{mso-style-parent:style0;
-	font-weight:700;
-	mso-number-format:"0\.000";
-	text-align:center;}
-.xl69
-	{mso-style-parent:style18;
-	color:#FA7D00;
-	font-weight:700;
-	mso-number-format:"0\.000";
-	text-align:center;
-	border:.5pt solid #7F7F7F;
-	background:#F2F2F2;
-	mso-pattern:black none;}
-.xl70
-	{mso-style-parent:style0;
-	font-size:10.0pt;
-	font-weight:700;
-	font-family:"Arial Unicode MS";
-	mso-generic-font-family:auto;
-	mso-font-charset:0;
-	vertical-align:middle;}
-.xl71
-	{mso-style-parent:style16;
-	color:#006100;
-	text-align:center;
-	border-top:none;
-	border-right:none;
-	border-bottom:1.0pt solid windowtext;
-	border-left:none;
-	background:#C6EFCE;
-	mso-pattern:black none;}
-.xl72
-	{mso-style-parent:style0;
-	text-align:center;}
-.xl73
-	{mso-style-parent:style0;
-	mso-number-format:"0\.000";
-	text-align:center;}
-.xl74
-	{mso-style-parent:style0;
-	font-size:18.0pt;
-	font-weight:700;
-	font-family:Sathu;
-	mso-generic-font-family:auto;
-	mso-font-charset:0;}
-
-</style>
-     <link type="text/css" rel="stylesheet" href="style.css" />
+<link type="text/css" rel="stylesheet" href="style.css" />
 </head>
 
 <body link=blue vlink=purple>
@@ -184,17 +34,17 @@ echo "$line";
     ?>  
 
 	
-<table border=0 cellpadding=0 cellspacing=0 width=1097 style='border-collapse: collapse;table-layout:fixed;width:1097pt'>
+<table class="rating-biases-table" border=0 cellpadding=0 cellspacing=0 width=1097 style='border-collapse: collapse;table-layout:fixed;width:1097pt'>
 
- <col width=114 style='mso-width-source:userset;mso-width-alt:4864;width:114pt'>
- <col width=116 style='mso-width-source:userset;mso-width-alt:4949;width:116pt'>
- <col width=27 style='mso-width-source:userset;mso-width-alt:1152;width:27pt'>
- <col width=71 span=6 style='mso-width-source:userset;mso-width-alt:3029;width:71pt'>
- <col width=21 style='mso-width-source:userset;mso-width-alt:896;width:21pt'>
- <col class=xl68 width=131 span=3 style='mso-width-source:userset;mso-width-alt: 5589;width:131pt'>
+ <col width=114 style='width:114pt'>
+ <col width=116 style='width:116pt'>
+ <col width=27 style='width:27pt'>
+ <col width=71 span=6 style='width:71pt'>
+ <col width=21 style='width:21pt'>
+ <col class=xl68 width=131 span=3 style='width:131pt'>
 
  <tr height=23 style='height:23.0pt'>
-  <td height=23 class=xl70 colspan=4 width=328 style='height:23.0pt;mso-ignore:  colspan;width:328pt'>PlayoffPredictor.com rating biases for 2024 season:<span style="mso-spacerun:yes">&nbsp;&nbsp;&nbsp;</span></td>
+  <td height=23 class=xl70 colspan=4 width=328 style='height:23.0pt;width:328pt'>PlayoffPredictor.com rating biases for 2024 season:</td>
  </tr>
  <tr>
   <td style='background-color:ffcccc'>  High (positive) numbers mean that the playoff committee values a team more than the computer values that team (team is overrated by committee)</td>
@@ -232,24 +82,14 @@ echo "$line";
 
 
 
- <tr height=14 style='height:14.0pt'>
-  <td height=14 colspan=10 style='height:14.0pt;mso-ignore:colspan'></td>
-  <td class=xl68></td>
-  <td class=xl68></td>
-  <td class=xl68></td>
- </tr>
- <tr height=14 style='height:14.0pt'>
-  <td height=14 colspan=10 style='height:14.0pt;mso-ignore:colspan'></td>
-  <td class=xl68></td>
-  <td class=xl68></td>
-  <td class=xl68></td>
- </tr>
+ <tr height=14 style='height:14.0pt'></tr>
+ <tr height=14 style='height:14.0pt'></tr>
 
 <tr height=16 style='height:16.0pt'>
   <td height=16 class=xl70 style='height:16.0pt'>&nbsp;</td>
   <td colspan=8 class=xl71 style='background-color:cceeff'    >Rating Biases</td>
   <td></td>
-  <td class=xl65></td>
+  <td class=xl65></td>  <!-- this makes a yellow bar above the labels for each column -->
   <td class=xl65></td>
   <td class=xl65></td>
  </tr>
@@ -276,51 +116,51 @@ echo "$line";
  
  
  
- 	<?php
+<?php
+
 $fullSeasonBias = '/home/neville/cfbPlayoffPredictor/data/2024/fullSeasonCommitteeBiasMatrix.txt';
 $data = file_get_contents("$fullSeasonBias");
 
 $lineFromText = explode("\n", $data);
 $row=0;
 foreach($lineFromText as $line) {
-		$biasData = explode(":", $line);
 
-		#here I should round everything to 3 significant digits, except for 0. ToDo.
-		#if ((length($biasData[8])) == 6 ) {
-		#$biasData[8] = substr ($biasData[8],0,5);   # want this to 3 significant digits, not 4
-		#}
+	if ($line == '') { #ignore empty last line
+		continue;
+	}
+
+	$biasData = explode(":", $line);
+
+	#here I should round everything to 3 significant digits, except for 0. ToDo.
+	#if ((length($biasData[8])) == 6 ) {
+	#$biasData[8] = substr ($biasData[8],0,5);   # want this to 3 significant digits, not 4
+	#}
 
 		
-		if ($biasData[8] > .03) {echo " <tr height=15 style='height:15.0pt; background-color:ffcccc'    >";}  
-		elseif ($biasData[8] < -.03)  {echo " <tr height=15 style='height:15.0pt; background-color:f7ffe6'    >";}
-		else  {echo " <tr height=15 style='height:15.0pt; background-color:f2f2f2'    >";}
+	if ($biasData[8] > .03) {echo " <tr height=15 style='height:15.0pt; background-color:ffcccc'    >";}  
+	elseif ($biasData[8] < -.03)  {echo " <tr height=15 style='height:15.0pt; background-color:f7ffe6'    >";}
+	else  {echo " <tr height=15 style='height:15.0pt; background-color:f2f2f2'    >";}
 
-		echo " <td height=15 style='height:15.0pt'>$biasData[0]<span style=\"mso-spacerun:yes\">&nbsp;</span></td>  ";
-		echo "  <td class=xl73>$biasData[8]</td>  ";
-		echo "  <td class=xl74></td> "; 
-		echo "  <td class=xl73>$biasData[1]</td> ";
-		echo "  <td class=xl73>$biasData[2]</td> ";
-		echo "  <td class=xl73>$biasData[3]</td> ";
-		echo "  <td class=xl73>$biasData[4]</td> ";
-		echo "  <td class=xl73>$biasData[5]</td> ";
-		echo "  <td class=xl73>$biasData[6]</td> "; 
+	echo " <td height=15 style='height:15.0pt'>$biasData[0]</td>  ";
+	echo "  <td class=xl73>$biasData[8]</td>  ";
+	echo "  <td class=xl74></td> "; 
+	echo "  <td class=xl73>$biasData[1]</td> ";
+	echo "  <td class=xl73>$biasData[2]</td> ";
+	echo "  <td class=xl73>$biasData[3]</td> ";
+	echo "  <td class=xl73>$biasData[4]</td> ";
+	echo "  <td class=xl73>$biasData[5]</td> ";
+	echo "  <td class=xl73>$biasData[6]</td> "; 
 	#	echo "  <td class=xl73>$biasData[7]</td> ";   #todo - need sheet formatting before I can use this
-		echo "  <td class=xl73></td> ";
-		echo "  <td class=xl73>$biasData[9]</td> ";
-		echo "  <td class=xl73>$biasData[10]</td> ";
-		echo "  <td class=xl73>$biasData[11]</td> ";
-		echo "  </tr> ";
-		}
+	echo "  <td class=xl73></td> ";
+	echo "  <td class=xl73>$biasData[9]</td> ";
+	echo "  <td class=xl73>$biasData[10]</td> ";
+	echo "  <td class=xl73>$biasData[11]</td> ";
+	echo "  </tr> ";
+	}
 
  	
- 	?>
+?>
  
-
-
- 
- 
-
-
 
 
 
